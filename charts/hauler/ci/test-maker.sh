b119ef6457cd0023bb86f51edb6a643bf63f8ab9
Taylor Shrauner-Biggs <taylor.shrauner-biggs@ranchergovernment.com>
2025-02-07 17:55:26 -0600

    push test outputs and start to use them

---
+++ charts/hauler/ci/test-maker.sh
@@ -0,0 +1,12 @@
+#for testvalue in $(ls *-values.yaml)
+#do
+#  name=$(basename ${testvalue}| rev | cut -c13- | rev)
+#  namespace=hauler-${name}
+#  helm install $name .. --namespace ${namespace} --values ${testvalue} --create-namespace
+#  REGISTRY_URL=http://hauler-registry.${namespace}.svc.cluster.local:5000/v2/_catalog
+#  kubectl exec -q test-pod -- curl -s ${REGISTRY_URL} > ${name}-reg-output.txt
+#  HTTP_URL=http://hauler-fileserver.${namespace}.svc.cluster.local:8080
+#  kubectl exec -q test-pod -- curl -s ${HTTP_URL} > ${name}-web-output.txt
+#done
+
+#exit
