3b17f63362de304835c8dab9db59293aa9af16b1
Taylor Shrauner-Biggs <taylor.shrauner-biggs@ranchergovernment.com>
2025-02-07 18:23:58 -0600

    update test results since manifests were changed, cleaner paths

--- charts/hauler/ci/test-maker.sh
+++ charts/hauler/ci/test-maker.sh
@@ -3,10 +3,9 @@
 #  name=$(basename ${testvalue}| rev | cut -c13- | rev)
 #  namespace=hauler-${name}
 #  helm install $name .. --namespace ${namespace} --values ${testvalue} --create-namespace
+#  helm uninstall $name --namespace ${namespace}
 #  REGISTRY_URL=http://hauler-registry.${namespace}.svc.cluster.local:5000/v2/_catalog
-#  kubectl exec -q test-pod -- curl -s ${REGISTRY_URL} > ${name}-reg-output.txt
+#  kubectl exec -q test-pod -- curl -s ${REGISTRY_URL} > expected/${name}-reg-output.txt
 #  HTTP_URL=http://hauler-fileserver.${namespace}.svc.cluster.local:8080
-#  kubectl exec -q test-pod -- curl -s ${HTTP_URL} > ${name}-web-output.txt
+#  kubectl exec -q test-pod -- curl -s ${HTTP_URL} > expected/${name}-web-output.txt
 #done
-
-#exit
